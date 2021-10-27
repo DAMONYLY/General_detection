2b6dd3143013d8f34c490544191b8e0350689e65
damonring <742141957@qq.com>
2021-10-27 19:27:18 +0800

    update Mish and Swish

--- model/layers/activate.py
+++ model/layers/activate.py
@@ -5,16 +5,15 @@ import torch.nn.functional as F
 
 class Mish(nn.Module):
     def __init__(self):
-        super(Mish).__init__()
-
+        super().__init__()
+        # print("Mish activation loaded...")
     def forward(self, x):
         x = x * (torch.tanh(F.softplus(x)))
         return x
 
-
 class Swish(nn.Module):
     def __init__(self):
-        super(Swish, self).__init__()
+        super().__init__()
 
     def forward(self, x):
         x = x * F.sigmoid(x)
